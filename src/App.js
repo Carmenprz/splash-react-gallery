7fcb331a5f74a5b9799c3e17ddbfba104c21eaf0
Carmen <carmenp.gomez99@gmail.com>
2022-04-01 22:17:55 +0200

    feat(grid): gallery grid created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './scss/_reset.scss';
+import './App.scss';
 import Nav from './components/Nav'
+import Gallery from './components/Gallery'
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Gallery />
     </div>
   );
 }
---
+++ src/components/Gallery/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles.scss';
+
+
+export default function Gallery() {
+    return (
+        <article className='gallery'>
+            <div className='gallery__picture'></div>
+            <div className='gallery__picture'></div>
+            <div className='gallery__picture'></div>
+            <div className='gallery__picture'></div>
+            <div className='gallery__picture'></div>
+            <div className='gallery__picture'></div>
+            <div className='gallery__picture'></div>
+            <div className='gallery__picture'></div>
+        </article>
+    )
+}
